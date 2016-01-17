b4db9cd517472ad619a6a1b1afe2943774831419
jeffthemaximum <frey.maxim@gmail.com>
2016-01-17 10:01:54 -0500

    bunny ears

---
+++ challenge_001/bunny_ears.py
@@ -0,0 +1,19 @@
+'''
+Bunny Ears
+You have a bunch of bunnies (1, 2, ..., n).
+Each even (n = 2, etc) bunny has 3 ears (maybe they have a foot in the air), 
+each odd bunny has 2 ears.
+
+Return the number of total ears in a group of bunnies using recursion.
+Here are example outputs.
+
+bunny_ears(0) => 0
+bunny_ears(1) => 2
+bunny_ears(2) => 5
+bunny_ears(3) => 7
+Write some tests first. Make sure to test for different scenarios. What if you 
+get a negative number? How about zero?
+
+If you're having problems conceptualizing the problem, feel free to solve this 
+using loops first, but, afterwards, solve it again using recursion.
+'''
